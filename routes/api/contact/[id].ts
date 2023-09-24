bb7e20563470fae553d304998a8393e7a709759e
Jacob Stovall <jacobemmersonstovall@gmail.com>
2023-09-24 12:46:17 -0700

    Change the api to match format of data stored

--- routes/api/contact/[id].ts
+++ routes/api/contact/[id].ts
@@ -7,21 +7,17 @@ interface Result {
     value: string;
 }
 
-interface Contact {
-    value: string;
-}
-
 export const handler: Handlers = {
     async GET(_req, ctx) {
         console.log(_req.url);
         const id = ctx.params.id;
         const key = ["contact", id];
-        const contact = (await kv.get<Contact>(key)).value;
+        const contact = (await kv.get<string>(key)).value;
         const resp : Result = { successful: false, value: "Not a valid contact command" };
         if(contact != null)
         {
             resp.successful = true;
-            resp.value = contact.value;
+            resp.value = contact;
         }
         return new Response(JSON.stringify(resp));
     }
